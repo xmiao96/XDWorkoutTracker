6bd65e2cde4f8cd365ff85b54bf19f0a6ac08110
xuxi <xuxi.huang@hotmail.com>
2022-03-24 15:21:30 -0700

    made progress and alarm screens

---
+++ app/src/main/java/com/example/xdworkouttracker/AlarmEditActivity.java
@@ -0,0 +1,14 @@
+package com.example.xdworkouttracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlarmEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_edit);
+    }
+}
